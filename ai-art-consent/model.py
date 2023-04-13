ea594e3d9ac5472ae26cfb1d06e6485aa755a93a
Jasen Qin <jiasheng.qin@student.unsw.edu.au>
2023-04-14 03:33:17 +1000

    Quick Updates

---
+++ ai-art-consent/model.py
@@ -0,0 +1,61 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+# A simple image embedding
+class ImageEmbedding(nn.Module):
+    def __init__(self, input_channels, embed_dim):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(input_channels, embed_dim, 3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+# A simple diffusion model
+class MiniStableDiffusion(nn.Module):
+    def __init__(self, embed_dim, artist_dim):
+        super().__init__()
+        self.embedding = ImageEmbedding(3, embed_dim)
+        self.fc1 = nn.Linear(embed_dim + artist_dim, 256)
+        self.fc2 = nn.Linear(256, embed_dim)
+
+    def forward(self, x, artist_id):
+        x_embed = self.embedding(x)
+        x_flatten = x_embed.view(x_embed.size(0), -1)
+        x_concat = torch.cat((x_flatten, artist_id), dim=1)
+        x_fc1 = torch.relu(self.fc1(x_concat))
+        x_out = self.fc2(x_fc1)
+
+        return x_out
+
+# Hyperparameters
+embed_dim = 64
+artist_dim = 16
+learning_rate = 0.001
+num_epochs = 10
+
+# Initialize the model, loss function and optimizer
+model = MiniStableDiffusion(embed_dim, artist_dim)
+criterion = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+# Dummy input image and artist ID
+input_image = torch.randn(1, 3, 64, 64)
+artist_id = torch.randn(1, artist_dim)
+
+# Training loop
+for epoch in range(num_epochs):
+    optimizer.zero_grad()
+    output = model(input_image, artist_id)
+    loss = criterion(output, input_image.view(input_image.size(0), -1))
+    loss.backward()
+    optimizer.step()
+    print(f'Epoch: {epoch+1}, Loss: {loss.item()}')
+
+# Generate output image and list of contributing artist IDs
+output_image = model(input_image, artist_id)
+contributing_artists = [artist_id]
