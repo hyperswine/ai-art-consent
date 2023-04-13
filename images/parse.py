505b4da4a2ea450d05a49099f76bce68d05197b2
Jasen Qin <jiasheng.qin@student.unsw.edu.au>
2023-04-14 04:09:42 +1000

    Add

--- images/parse.py
+++ images/parse.py
@@ -3,19 +3,20 @@ from PIL import Image
 from io import BytesIO
 
 
-def parse_art_file(input_image_path = "output_image.art") -> Image.Image:
+def parse_art_file(input_image_path="output_image.art") -> Image.Image:
     with open(input_image_path, "rb") as f:
         data = f.read()
 
     # Find the start of the metadata
     metadata_start = data.find(b'{')
+    metadata_end = data.find(b'}')
 
     # Load the image
     image_data = data[:metadata_start]
     img = Image.open(BytesIO(image_data))
 
     # Load the metadata
-    metadata_str = data[metadata_start:].decode("utf-8")
+    metadata_str = data[metadata_start:metadata_end].decode("utf-8")
     metadata = json.loads(metadata_str)
 
     # Print the metadata
